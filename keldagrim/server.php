ad6205c7b69fbb0259bef47490a0554b9ae495a9
last.lantican <lastdavid.lantican@gmail.com>
2026-06-27 01:32:56 +0800

    add oracle server request logging

--- keldagrim/server.php
+++ keldagrim/server.php
@@ -1,5 +1,7 @@
 <?php
 
+$start = $_SERVER['REQUEST_TIME_FLOAT'] ?? microtime(true);
+
 require_once __DIR__ . '/Autoloader.php';
 Autoloader::init();
 
@@ -17,6 +19,27 @@ if (
   file_exists($static_file) &&
   !is_dir($static_file)
   ) return false;
+
+register_shutdown_function(function() use ($start, $request_uri) {
+  $time = (new DateTimeImmutable)->format(DateTimeInterface::ATOM);
+  $ip = $_SERVER['REMOTE_ADDR'] ?? '-';
+  $method = $_SERVER['REQUEST_METHOD'] ?? '-';
+  $status = http_response_code();
+  $duration = round((microtime(true) - $start) * 1000, 2);
+
+  file_put_contents(
+    "php://stderr", 
+    sprintf(
+      "%s %-15s %-6s %-40s %3d %7.2fms\n", 
+      $time, 
+      $ip,
+      $method, 
+      $request_uri, 
+      $status, 
+      $duration
+    )
+  );
+});
 
 require_once 
   Config::HOME_DIR() . DIRECTORY_SEPARATOR . 
