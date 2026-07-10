618ed55286ccbcc9678792402b960a6330932238
last.lantican <lastdavid.lantican@gmail.com>
2026-07-10 18:09:21 +0800

    add exceptions

---
+++ keldagrim/throwable/exception/invalidargument/ActionViewException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Keldagrim\Throwable\Exception\InvalidArgument;
+
+use Keldagrim\Throwable\Exception\KeldagrimInvalidArgumentException;
+
+class ActionViewException extends KeldagrimInvalidArgumentException {
+
+}
---
+++ keldagrim/throwable/exception/logic/ActionViewException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Keldagrim\Throwable\Exception\Logic;
+
+use Keldagrim\Throwable\Exception\KeldagrimLogicException;
+
+class ActionViewException extends KeldagrimLogicException {
+
+}
