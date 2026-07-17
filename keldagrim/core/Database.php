0a74f10af217d32259cc995afb855efdec5dd97b
last.lantican <lastdavid.lantican@gmail.com>
2026-07-17 13:01:55 +0800

    update database class

--- keldagrim/core/Database.php
+++ keldagrim/core/Database.php
@@ -40,7 +40,7 @@ final class Database {
     };
 
     try {
-      $pdo = new \PDO($dsn, $username, $password ?? '', [
+      $pdo = new \PDO($dsn, $username, $password, [
         \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES   => false,
